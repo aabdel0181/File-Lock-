ba001c6d6e6aee4a2ce2e43fc893a32dc52c29f5
seanw <seanwjcho@gmail.com>
2023-11-12 02:52:35 +0300

    added needed imports + improved marketplace

--- hardhat/contracts/marketplace.sol
+++ hardhat/contracts/marketplace.sol
@@ -1,77 +1,195 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "../node_modules/@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "../node_modules/@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "../node_modules/@bnb-chain/greenfield-contracts/contracts/interface/IERC721Nontransferable.sol";
+import "../node_modules/@bnb-chain/greenfield-contracts/contracts/interface/IERC1155Nontransferable.sol";
+import "./BucketApp.sol";
+import "./ObjectApp.sol";
+import "./GroupApp.sol";
 
-contract Marketplace {
-    using SafeERC20 for IERC20;
+interface IERC1155Mintable {
+    function mint(address to, uint256 id, uint256 amount, bytes memory data) external;
+}
 
-    address public admin;
-    IERC20 public token;  // Assuming you have a BNB-based token
+contract Marketplace is AccessControl, BucketApp, ObjectApp, GroupApp {
 
-    struct Listing {
-        address creator;
-        string fileHash;
-        uint256 price;
-        bool isAvailable;
-    }
+    // greenfield system contracts
+    address public constant CROSS_CHAIN = 0xa5B2c9194131A4E0BFaCbF9E5D6722c873159cb7;
+    address public constant GROUP_HUB = 0x50B3BF0d95a8dbA57B58C82dFDB5ff6747Cc1a9E;
+    address public constant GROUP_TOKEN = 0x7fC61D6FCA8D6Ea811637bA58eaf6aB17d50c4d1;
+    address public constant MEMBER_TOKEN = 0x43bdF3d63e6318A2831FE1116cBA69afd0F05267;
 
-    mapping(uint256 => Listing) public listings;
-    uint256 public nextListingId;
-    mapping(address => uint256) public balances;
+    mapping(uint256 => uint256) objectPrice; //object id to price mapping
+    mapping(uint256 => string) objectName; //object id to name mapping
+    mapping(uint256 => uint256) objectGroup; //object id to group id mapping
+    mapping(uint256 => address) objectOwner; //object id to seller mapping;
+    mapping(uint256 => address) objectBucket; //object id to bucketmapping;
+    mapping(uint256 => string) groupName; //group id to name mapping;
+    mapping(uint256 => address) groupOwner; //group owner to seller mapping;
+    mapping(string => address) bucketOwner; //group
+   
 
-    event ListingCreated(uint256 listingId, address creator, string fileHash, uint256 price);
-    event ListingPurchased(uint256 listingId, address buyer, uint256 price);
+    mapping(address => uint256) public balances; //Keeps track of all seller balances
+
 
     modifier onlyAdmin() {
         require(msg.sender == admin, "Not admin");
         _;
     }
 
-    constructor(address _admin, address _token) {
-        admin = _admin;
-        token = IERC20(_token);
+    function initialize(
+        address _initAdmin,
+        address _fundWallet,
+        uint256 _feeRate,
+        uint256 _callbackGasLimit,
+        uint8 _failureHandleStrategy
+    ) public initializer {
+        require(_initAdmin != address(0), "MarketPlace: invalid admin address");
+        _grantRole(DEFAULT_ADMIN_ROLE, _initAdmin);
+
+        transferGasLimit = 2300;
+        fundWallet = _fundWallet;
+        feeRate = _feeRate;
+
+        __base_app_init_unchained(CROSS_CHAIN, _callbackGasLimit, _failureHandleStrategy);
+        __group_app_init_unchained(GROUP_HUB);
     }
 
-    function createListing(string memory _fileHash, uint256 _price) external {
-        require(_price > 0, "Price must be greater than 0");
-        //now, create watermarked version. 
-        //upload both to BNB Greenfield
+    function greenfieldCall(
+        uint32 status,
+        uint8 resourceType,
+        uint8 operationType,
+        uint256 resourceId,
+        bytes calldata callbackData
+    ) external override(BucketApp, ObjectApp, GroupApp) {
+        require(
+            msg.sender == bucketHub || msg.sender == objectHub || msg.sender == groupHub,
+            "invalid caller"
+        );
 
-        uint256 listingId = nextListingId;
-        nextListingId++;
-
-        listings[listingId] = Listing({
-            creator: msg.sender,
-            fileHash: _fileHash,
-            price: _price,
-            isAvailable: true
-        });
-
-        emit ListingCreated(listingId, msg.sender, _fileHash, _price);
+        if (resourceType == RESOURCE_BUCKET) {
+            _bucketGreenfieldCall(status, operationType, resourceId, callbackData);
+        } else if (resourceType == RESOURCE_OBJECT) {
+            _objectGreenfieldCall(status, operationType, resourceId, callbackData);
+        } else if (resourceType == RESOURCE_GROUP) {
+            _groupGreenfieldCall(status, operationType, resourceId, callbackData);
+        } else {
+            revert(string.concat("EbookShop: ", ERROR_INVALID_RESOURCE));
+        }
     }
 
-    function purchaseListing(uint256 _listingId) external {
-        Listing storage listing = listings[_listingId];
+    function list(uint256 _objectId, uint256 _price) external { //good
+        require(
+            IERC721NonTransferable(objectToken).ownerOf(_objectId) == msg.sender,
+            "must be owner"
+        );
+        require(_price > 0);
+        //require sender owns token
+        objectPrice[_objectId] = _price; //assign price to tokenId
+        IERC1155Mintable(ebookToken).mint(msg.sender, _ebookId, 1, ""); //mints ownership token to creator
+    }
 
-        require(listing.isAvailable, "Listing not available");
-        require(token.balanceOf(msg.sender) >= listing.price, "Insufficient funds");
+    function createGroup(uint256 _objectId) public payable { //good
+        require(
+            IERC721NonTransferable(objectToken).ownerOf(_objectId) == msg.sender,
+            "must be owner"
+        );
+        string memory name = string.concat("Purchasers of ", objectName[_objectId]);
+        _createGroup(owner, name); //create group with owner 
+    }
 
-        token.safeTransferFrom(msg.sender, listing.creator, listing.price);
-        balances[listing.creator] += listing.price;
-        listing.isAvailable = false;
+    function purchaseListing(uint256 _objectId) external payable {
+        require(objectPrice[_objectId] > 0, "not listed yet");
 
-        //now assign ownership to purchaser on greenfield.
-        //provide NFT proof of ownership.
+        uint256 price = objectPrice[_objectId];
+        require(msg.value >= price, "not enough payment");
+
+        IERC1155Mintable(objectToken).mint(msg.sender, _objectId, 1, ""); //what is objectToken //sends proof of ownership to buyer.
+
+        uint256 _groupId = objectGroup[_objectId];
+        address _owner = IERC721NonTransferable(groupToken).ownerOf(_groupId);
+
+        address[] memory _member = new address[](1);
+        uint64[] memory _expiration = new uint64[](1);
+
+        _member[0] = msg.sender;
+        _expiration[0] = 0;
+        _updateGroup(_owner, _groupId, GroupStorage.UpdateGroupOpType.AddMembers, _member, _expiration); //updates group permissions to include buyer, they can now download file
+
+        balances[_owner] += price;
 
         emit ListingPurchased(_listingId, msg.sender, listing.price);
     }
 
-    function disburseFunds(uint256 _amount) external {
+    function disburseFunds(uint256 _amount) external { //take fee here
         require(_amount > 0, "Amount must be greater than 0");
         require(balances[msg.sender] >= _amount);
         balances[msg.sender] -= amount;
         token.safeTransfer(msg.sender, _amount);
+    }
+
+
+    /////REGISTER ITEMS
+
+    /**
+     * @dev Register group resource that mirrored from GreenField to BSC.
+     */
+    function registerGroup(string calldata _groupName, uint256 tokenId) external {
+        require(
+            IERC721NonTransferable(groupToken).ownerOf(tokenId) == msg.sender,
+            string.concat("EbookShop: ", ERROR_INVALID_CALLER)
+        );
+        require(bytes(name).length > 0, "invalid name");
+        require(groupId[name] == 0, "group exists");
+
+        groupName[tokenId] = name;
+        groupId[name] = tokenId;
+    }
+
+    /**
+     * @dev Register object resource that mirrored from GreenField to BSC.
+     */
+    function registerObject(
+        string calldata _objectName,
+        uint256 _objectId,
+        string calldata _groupName,
+        uint256 _groupId
+    ) external {
+        require(
+            IERC721NonTransferable(objectToken).ownerOf(_objectId) == msg.sender,
+            "invalid caller"
+        );
+        require(bytes(_objectName).length > 0, "invalid name");
+        require(objectId[_objectName] == 0, "name already exists");
+
+        objectOwner[_objectName] = objectId;
+
+        if (_groupId != 0) {
+            require(
+                IERC721NonTransferable(groupToken).ownerOf(_groupId) == msg.sender,
+                "invalid caller"
+            );
+            require(bytes(_groupName).length > 0, "invalid group name");
+
+            groupName[_groupId] = _groupName;
+            groupId[_groupName] = _groupId;
+
+            groupEbook[_groupId] = _ebookId;
+            ebookGroup[_ebookId] = _groupId;
+        }
+    }
+
+    /**
+     * @dev Register bucket resource that mirrored from GreenField to BSC.
+     */
+    function registerGroup(string calldata name, uint256 tokenId) external {
+        require(
+            IERC721NonTransferable(groupToken).ownerOf(tokenId) == msg.sender,
+            "invalid caller"
+        );
+        require(bytes(name).length > 0, "invalid name");
+        bucketOwner[tokenId] = msg.sender;
     }
 }
