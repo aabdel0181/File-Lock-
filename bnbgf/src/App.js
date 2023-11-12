76abd9b37e8b156e66c6465607037b3095a4acc8
Yun Jae Shin <joeyshin@sas.upenn.edu>
2023-11-12 04:45:38 +0300

    Update data

--- bnbgf/src/App.js
+++ bnbgf/src/App.js
@@ -6,57 +6,33 @@ import Navbar from './Navbar';
 import Home from './Home'; 
 import Marketplace from './Marketplace';
 import StoreData from './StoreData';
-
+import Signin from './Signin';
 import Web3 from 'web3';
-import { web3 } from './wallet';
+
+
+export const web3 = new Web3(window.ethereum);
+
 
 function App() {
-
   const [account, setAccount] = useState(null);
-  const connectWallet = async () => {
-    const web3 = window.ethereum ? new Web3(window.ethereum) : null;
-
-    if(!web3) {
-      alert('Please Install Metamask');
-      return; 
-    }
-
-    try {
-      const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });  
-      setAccount(accounts[0]);
-    } catch(err) {
-      console.error(err);
-    }
-  }
-
-  // const isConnected = async () => {
-  //   const web3 = new Web3(window.ethereum);
-  //   const accounts = await web3.eth.getAccounts();
-  //   console.log(accounts);
-  //   return accounts && accounts.length > 0;
-  // };
-
 
   useEffect(() => {
     const getAccount = async () => {
       const accounts = await web3.eth.getAccounts();
       setAccount(accounts[0]);
     }
+
     getAccount();
   }, []);
 
   return (
     <>
     {!account ? 
-    <div className="modal">
-      <p>Please sign in with MetaMask</p>
-      <button onClick={connectWallet}> Connect Wallet </button>
-    </div>
+      <Signin />
     : 
       <BrowserRouter>
         <Navbar />
         <Routes>
-
           <Route path="/" element={<Home />} />
           <Route path="/store-data" element={<StoreData />} />
           <Route path="/marketplace" element={<Marketplace />} />
--- bnbgf/src/Signin.js
+++ bnbgf/src/Signin.js
@@ -1,11 +1,54 @@
 import React from 'react'
+import { Button, Box, Typography, Card, CardContent, CardMedia, CardActions } from '@mui/material';
+import Web3 from 'web3';
+import metamaskImg from './metamask.png';
 
-const Signin = ({children}) => {
-  return (
-    <div className="modal">
-      {children}
-    </div>
-  )
+
+const Signin = () => {
+    const connectWallet = async () => {
+        const web3 = window.ethereum ? new Web3(window.ethereum) : null;
+
+        if(!web3) {
+            alert('Please Install Metamask');
+            return; 
+        }
+        try {
+            const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });  
+            window.location.reload()
+        } catch(err) {
+            console.error(err);
+        }
+    }
+    
+      return (
+        <Box sx={{
+            height: '100vh',
+            display: 'flex', 
+            alignItems: 'center',
+            justifyContent: 'center',
+            background: 'linear-gradient(#f4ede3, #e9dccc)'
+         }}>
+            <Card sx={{ maxWidth: 345, padding: '50px'}}>
+                <CardMedia
+                    sx={{
+                        // opacity: 0.7,
+                        // position: 'absolute',
+                        // top: -50, 
+                        // left: '50%',
+                        // transform: 'translateX(-50%)'
+                    }}
+                    component="img"
+                    alt="metamask"
+                    height="140"
+                    image={metamaskImg}
+                />
+                <CardContent sx={{ p: 3 }} />
+                <CardActions sx={{ justifyContent: 'center' }}>
+                    <Button size="medium" onClick = {connectWallet}>Connect your wallet</Button>
+                </CardActions>
+            </Card>
+        </Box>
+      )
 }
 
-export default Signin;
+export default Signin
